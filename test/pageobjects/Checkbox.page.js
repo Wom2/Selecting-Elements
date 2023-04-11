162c15abae5f2cd4f1681c37b3e5362a92a319a7
MacMini21 <macmini21@MacMini21s-Mac-mini.local>
2023-04-11 16:00:58 -0700

    in progress

--- test/pageobjects/Checkbox.page.js
+++ test/pageobjects/Checkbox.page.js
@@ -2,7 +2,7 @@ import Page from './page.js';
 
 class CheckboxPage extends Page {
     get checkbox1 () {
-        return $('*[@id="checkboxes"]/input[1]');
+        return $('#checkboxes > input[type=checkbox]:nth-child(1)');
     }
     get checkbox2 () {    
         return $('//form/input[2]');
--- test/specs/Checkbox.spec.js
+++ test/specs/Checkbox.spec.js
@@ -3,5 +3,13 @@ import CheckboxPage from '../pageobjects/Checkbox.page.js'
 describe('My Checkbox application', () => {
     it('should click both boxes', async () => {
         await CheckboxPage.open()
+        await (CheckboxPage).checkbox1.click()
+        await (browser).pause(4000)
+        await (CheckboxPage).checkbox1.isSelected()
+        //I am currently trying to get true and false to function
+        //for isSelected.maybe change to expect to be selected
+        await (CheckboxPage).checkbox2.click()
+        await (browser).pause(4000)
+        await (CheckboxPage).checkbox2.isSelected()
     })
 })
